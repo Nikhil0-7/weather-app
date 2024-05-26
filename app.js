bafe9fd855228126eac7db1be5d82dba5383b08f
Nikhil Kumar <85381110+Nikhil0-7@users.noreply.github.com>
2024-05-26 12:31:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+let searchButton = document.getElementById('search-button');
+
+searchButton.onclick=()=>fetchWeather();
+async function fetchWeather() {
+    const city = document.getElementById('city-input').value;
+    const apiKey = '08509ad5753a874aef3a069cb5f97500'; // Replace with your OpenWeatherMap API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (data.cod === 200) {
+            displayWeather(data);
+        } else {
+            alert('City not found');
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('weather-info');
+    weatherInfo.innerHTML = `
+        <h2>Location: ${data.name}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} Km/hr</p>
+        <p>Pressure: ${data.main.pressure} Pascal</p>
+        <p>Sunrise: ${new Date(data.sys.sunrise * 1000).toLocaleTimeString()}</p>
+        <p>Sunset: ${new Date(data.sys.sunset * 1000).toLocaleTimeString()}</p>
+    `;
+}
